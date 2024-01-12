31b0c1ca11800675b2af288ce8085c93665cac4c
TheHonoredOnes <156137944+TheHonoredOnes@users.noreply.github.com>
2024-01-12 23:54:37 +0100

    Update config.js

--- config.js
+++ config.js
@@ -9,13 +9,13 @@ import axios from 'axios'
 global.owner = [
   ['2349035391852', 'TheHonoredOne THO', true],
   ['2348123035565', 'Timothy', true], 
-  [''] 
+  ['2348077722045', 'crown', true] 
 ] //Number of owners
 
 //global.pairingNumber = "" //put your bot number here
 
 global.mods = ['2349035391852','32460248586'] 
-global.prems = ['2349035391852', '2348123035565']
+global.prems = ['2349035391852', '2348123035565','2348077722045']
 global.allowed = ['917605902011', '32460248586', '19152999993']
 global.keysZens = ['c2459db922', '37CC845916', '6fb0eff124']
 global.keysxxx = keysZens[Math.floor(keysZens.length * Math.random())]
